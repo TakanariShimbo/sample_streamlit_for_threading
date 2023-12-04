17fdeed183cade7fd7a93a270f5b9e6dccf8b310
shinbot <shinbot@uniontool.co.jp>
2023-12-04 17:29:46 +0900

    revise

--- components/sample.py
+++ components/sample.py
@@ -15,9 +15,12 @@ class Processer1(BaseProcesser):
     def pre_process(self):
         st.write("* PROCESSER 1")
         st.write("** START")
+        print("* PROCESSER 1")
+        print("** START")
 
     def post_process(self):
         st.write("** FINISH")
+        print("** FINISH")
 
 
 class Processer2(BaseProcesser):
@@ -29,22 +32,31 @@ class Processer2(BaseProcesser):
     def pre_process(self):
         st.write("* PROCESSER 2")
         st.write("** START")
+        print("* PROCESSER 2")
+        print("** START")
 
     def post_process(self):
         st.write("** FINISH")
+        print("** FINISH")
 
 
 class ProcessersManager(BaseProcessersManager):
+    def __add_message_area(self):
+        self.__message_area = st.empty()
+
     def pre_process_for_starting(self):
-        self.message_area = st.empty()
-        self.message_area.info("START")
+        self.__add_message_area()
+        self.__message_area.info("START")
+        print("---- START ----")
 
     def pre_process_for_running(self):
-        self.message_area = st.empty()
-        self.message_area.warning("ALREADY STARTED")
+        self.__add_message_area()
+        self.__message_area.warning("ALREADY STARTED")
+        print("---- ALREADY STARTED ----")
 
     def post_process(self):
-        self.message_area.info("FINISH")
+        self.__message_area.info("FINISH")
+        print("---- FINISH ----")
 
 
 class ProcessersManagerSState(BaseSState[ProcessersManager]):
