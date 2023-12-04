b81e9fb056475caea45d5e3675378f8b17204329
shinbot <shinbot@uniontool.co.jp>
2023-12-04 15:59:32 +0900

    revise

--- components/sample.py
+++ components/sample.py
@@ -7,7 +7,9 @@ from handlers.processers import BaseProcesser, BaseProcesserList
 
 class Processer1(BaseProcesser):
     def run(self):
-        sleep(5)
+        for i in range(5):
+            sleep(1)
+            print(i)
 
     def pre_process(self):
         st.write("* PROCESSER 1")
@@ -19,7 +21,9 @@ class Processer1(BaseProcesser):
 
 class Processer2(BaseProcesser):
     def run(self):
-        sleep(5)
+        for i in range(5):
+            sleep(1)
+            print(i)
 
     def pre_process(self):
         st.write("* PROCESSER 2")
@@ -43,9 +47,12 @@ class ProcesserList(BaseProcesserList):
 
 
 PROCESSER_LIST_STATE = "PROCESSER_LIST_STATE"
+
+
 def init_session_state():
     if not PROCESSER_LIST_STATE in st.session_state:
         st.session_state[PROCESSER_LIST_STATE] = ProcesserList([Processer1, Processer2])
+
 
 def display():
     init_session_state()
--- handlers/processers.py
+++ handlers/processers.py
@@ -60,57 +60,21 @@ class BaseProcesserList(ABC):
         return [processer_class() for processer_class in processer_class_list]
 
     def run_all(self):
+        # initialize processers if not running state
         if not self.__is_running:
             self.__processers = self.__init_processers(self.__processer_class_list)
 
+        # run pre-process
         if self.__is_running:
             self.pre_process_for_running()
         else:
             self.pre_process_for_starting()
 
+        # run main-processes
         self.__is_running = True
         for processer in self.__processers:
             processer.start_and_wait_to_complete()
         self.__is_running = False
 
+        # run post-process
         self.post_process()
-
-
-
-if __name__ == "__main__":
-    from time import sleep
-
-    class Processer1(BaseProcesser):
-        def run(self):
-            sleep(5)
-
-        def pre_process(self):
-            print("* PROCESSER 1")
-            print("** START")
-
-        def post_process(self):
-            print("** FINISH")
-
-    class Processer2(BaseProcesser):
-        def run(self):
-            sleep(5)
-
-        def pre_process(self):
-            print("* PROCESSER 2")
-            print("** START")
-
-        def post_process(self):
-            print("** FINISH")
-
-    class ProcesserList(BaseProcesserList):
-        def pre_process_for_starting(self):
-            print("---- START ALL ----")
-
-        def pre_process_for_running(self):
-            print("---- ERROR: ALREADY STARTED ----")
-
-        def post_process(self):
-            print("---- FINISH ALL ----")
-
-    processers = ProcesserList([Processer1, Processer2])
-    processers.run_all()
