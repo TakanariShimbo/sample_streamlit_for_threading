e75e36fb7cbebbb315ee41db52360a4428f9d6ec
TakanariShimbo <takanari.shimbo@gmail.com>
2023-12-07 00:05:02 +0900

    revise

--- app/sample/page.py
+++ app/sample/page.py
@@ -30,14 +30,18 @@ class SamplePage(BasePage):
         form_area = st.form(key="Form")
         with form_area:
             st.markdown("### Form")
-            _, left_area, _, right_area, _ = st.columns([1,3,1,3,1])
+            _, left_area, _, center_area, _, right_area, _ = st.columns([1,3,1,3,1,3,1])
             with left_area:
                 is_run_pushed = st.form_submit_button(label="RUN", type="primary", use_container_width=True)
+            with center_area:
+                is_rerun_pushed = st.form_submit_button(label="RERUN", type="primary", use_container_width=True)
             with right_area:
                 is_reset_pushed = st.form_submit_button(label="RESET", type="secondary", use_container_width=True)
         
         if is_run_pushed:
             ProcessersManagerSState.on_click_run(form_area=form_area)
+        elif is_rerun_pushed:
+            ProcessersManagerSState.on_click_rerun(form_area=form_area)
         elif is_reset_pushed:
             ProcessersManagerSState.on_click_reset()
 
