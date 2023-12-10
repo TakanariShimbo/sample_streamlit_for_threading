84f0366f06b7d46ad0e4525db214bf5006635afd
TakanariShimbo <takanari.shimbo@gmail.com>
2023-12-10 13:24:29 +0900

    revise

--- app/main/processers.py
+++ app/main/processers.py
@@ -3,6 +3,7 @@ from time import sleep
 import cv2
 import streamlit as st
 
+from .schema import FormSchema
 from .. import BaseProcesser, BaseProcessersManager
 
 
@@ -41,30 +42,37 @@ class Processer2(BaseProcesser):
 
 
 class ProcessersManager(BaseProcessersManager):
-    def set_message_area(self, form_area):
+    def set_form_area(self, form_area):
         with form_area:
-            self.__message_area = st.empty()
+            self.__form_area = st.empty()
 
-    def get_message_area(self):
-        return self.__message_area
+    def get_form_area(self):
+        return self.__form_area
+
+    def set_form_schema(self, form_schema: FormSchema):
+        self.__form_schema = form_schema
+
+    def get_form_schema(self) -> FormSchema:
+        return self.__form_schema
 
     def pre_process_for_starting(self, **kwargs):
-        self.set_message_area(form_area=kwargs["form_area"])
-        self.get_message_area().info("START")
+        self.set_form_area(form_area=kwargs["form_area"])
+        self.set_form_schema(form_schema=kwargs["form_schema"])
+        self.get_form_area().info("START")
         st.markdown("### Result")
         print("---- START ----")
 
     def pre_process_for_running(self, **kwargs):
-        self.set_message_area(form_area=kwargs["form_area"])
-        self.get_message_area().warning("RUNNING")
+        self.set_form_area(form_area=kwargs["form_area"])
+        self.get_form_area().warning("RUNNING")
         st.markdown("### Result")
         print("---- RUNNING ----")
 
     def post_process(self, **kwargs):
-        self.get_message_area().info("FINISH")
+        self.get_form_area().info("FINISH")
         st.image(
-            image=cv2.imread(filename=kwargs["form_schema"].image_filepath),
-            caption=kwargs["form_schema"].animal_type,
+            image=cv2.imread(filename=self.get_form_schema().image_filepath),
+            caption=self.get_form_schema().animal_type,
             channels="BGR",
         )
         st.balloons()
